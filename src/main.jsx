3b4acfc4d3e5a8daeab5dfadbb193404fd6b5114
YazarKemal <realistfakir1@gmail.com>
2025-07-02 01:38:01 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MetricFit from './components/MetricFit.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MetricFit />
+  </React.StrictMode>
+)
